981a7b8f6333902bc1e9ab42e77bfd90496685ba
mainga <49961392+mianga58@users.noreply.github.com>
2022-03-24 13:53:10 +0200

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http.response import StreamingHttpResponse
-from chat.camera import VideoCamera
+#from django.http.response import StreamingHttpResponse
+#from chat.camera import VideoCamera
 from django.core.exceptions import ObjectDoesNotExist
 from payment.models import Subscription
 
@@ -21,14 +21,14 @@ def forum(request):
         return render(request, 'payment/home.html')
     return render(request, 'chat/forum.html', )
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+#def gen(camera):
+   # while True:
+  #      frame = camera.get_frame()
+ #       yield (b'--frame\r\n'
+#				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-def video_feed(request):
-    return StreamingHttpResponse(gen(VideoCamera()),
-		content_type='multipart/x-mixed-replace; boundary=frame')
+#def video_feed(request):
+ #   return StreamingHttpResponse(gen(VideoCamera()),
+#		content_type='multipart/x-mixed-replace; boundary=frame')
 
